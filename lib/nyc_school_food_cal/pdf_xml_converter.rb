3ad5a70925889eac02f1cd85699aa4efb563002d
Micah Young <micah.young@gmail.com>
2018-01-21 04:28:18 -0500

    Disable gs output

--- lib/nyc_school_food_cal/pdf_xml_converter.rb
+++ lib/nyc_school_food_cal/pdf_xml_converter.rb
@@ -3,9 +3,9 @@ require 'nokogiri'
 
 class NycSchoolFoodCal::PdfXmlConverter
   def parse(pdf_content)
-    stdin, stdout = Open3.popen2('gs -sDEVICE=txtwrite -dTextFormat=1 -o - -')
+    stdin, stdout = Open3.popen2('gs -sDEVICE=txtwrite -dQUIET  -dTextFormat=1 -o - -')
     stdin.write(pdf_content)
     stdin.close
-    stdout.readlines.select {|line| line[/</]}.join('')
+    stdout.readlines.join('')
   end
 end
